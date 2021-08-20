44291367305fbeb8d9026c062e7309fe3a25cac4
YBJ <dqwdd@naver.com>
2021-08-20 11:05:39 +0900

    now StudentAdapter update ing 하고 변수명 연결 완료

--- app/src/main/java/com/example/listview_2021_08_20/adapters/StudentAdapter.kt
+++ app/src/main/java/com/example/listview_2021_08_20/adapters/StudentAdapter.kt
@@ -1,7 +1,11 @@
 package com.example.listview_2021_08_20.adapters
 
+import android.content.Context
 import android.widget.ArrayAdapter
 import com.example.listview_2021_08_20.datas.StudentData
 
-class StudentAdapter : ArrayAdapter<StudentData>(){
+class StudentAdapter(
+    val mContext : Context,
+    val resId : Int,
+    val mList : ArrayList<StudentData>) : ArrayAdapter<StudentData>(mContext, resId, mList){
 }
