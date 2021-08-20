531fd0d68f8756c7d69153f1be24d35f6423342f
YBJ <dqwdd@naver.com>
2021-08-20 10:56:28 +0900

    StudentData.kt class 생성 후 변수 값도 넣어봄 저기에도 변수값 선언 가능 보기 쉽게 엔터도 침

---
+++ app/src/main/java/com/example/listview_2021_08_20/datas/StudentData.kt
@@ -0,0 +1,8 @@
+package com.example.listview_2021_08_20.datas
+
+//StudentData() << 여기에도 변수 생성 가능
+class StudentData(
+    val name : String,
+    val birthYear : Int,
+    val address : String) {
+}
