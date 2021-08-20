3bb2bddadbf38cfea4f30fd8785fce86400b336b
YBJ <dqwdd@naver.com>
2021-08-20 11:02:40 +0900

    now StudentAdapter update ing

---
+++ app/src/main/java/com/example/listview_2021_08_20/adapters/StudentAdapter.kt
@@ -0,0 +1,7 @@
+package com.example.listview_2021_08_20.adapters
+
+import android.widget.ArrayAdapter
+import com.example.listview_2021_08_20.datas.StudentData
+
+class StudentAdapter : ArrayAdapter<StudentData>(){
+}
--- app/src/main/java/com/example/listview_2021_08_20/datas/StudentData.kt
+++ app/src/main/java/com/example/listview_2021_08_20/datas/StudentData.kt
@@ -5,4 +5,7 @@ class StudentData(
     val name : String,
     val birthYear : Int,
     val address : String) {
+
+
+
 }
